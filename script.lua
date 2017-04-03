238379703e1632d9c1294349eb354bb9499f93bf
megamilkx <gyrocopter3k@gmail.com>
2017-04-03 06:16:57 +0300

    Update test script

--- script.lua
+++ script.lua
@@ -1,9 +1,10 @@
 Foo3("Move", 12, 1.3, 3.7);
 
 o = GetObject(1);
-
-o.poop = 100013;
+o.poop = 13;
 o.msg = "Hello!";
+o.boob = 12.3;
+PrintObject(o);
 
 Print("Beepin' n' Boppin'");
 Print("That was really dumb")
